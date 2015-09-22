a19d9a486bf7df6d855983e8ad67484c10218a23
Ben <ben>
2015-09-22 16:33:52 +0100

    Work on sites with Content Security Policy headers. Some issues with certain sites.

--- content.js
+++ content.js
@@ -16,17 +16,32 @@ var pluginPrivacy = '(' + function () {
 		configurable: false,
 		enumerable: true,
 		writable: false
-	}
+	};
 	var navigator = Object.create(window.navigator);
 	Object.defineProperties(navigator, properties);
-	Object.defineProperty(window, 'navigator', {
-		value: navigator,
-		configurable: false,
-		enumerable: false,
-		writable: false
-	});
-	console.log("PluginPrivacy has removed the plugins and mimeTypes")
+	try {
+		Object.defineProperty(window, 'navigator', {
+			value: navigator,
+			configurable: false,
+			enumerable: false,
+			writable: false
+		});
+		console.log("PluginPrivacy has removed the plugins and mimeTypes", location.href)
+	} catch(e) {/*Cannot redefine property: navigator*/}
 } + ')();';
-document.documentElement.setAttribute('onreset', pluginPrivacy);
-document.documentElement.dispatchEvent(new CustomEvent('reset'));
-document.documentElement.removeAttribute('onreset');
+
+//GMail breaks. Probably some other apps will also break.
+if(!~location.href.indexOf('https://mail.google.com/_/scs')) {
+
+	//Without CSP
+	document.documentElement.setAttribute('onreset', pluginPrivacy);
+	document.documentElement.dispatchEvent(new CustomEvent('reset'));
+	document.documentElement.removeAttribute('onreset');
+
+	//With CSP
+	var script = document.createElement('script');
+	script.textContent = pluginPrivacy;
+	(document.head||document.documentElement).appendChild(script);
+	script.parentNode.removeChild(script);
+
+}
